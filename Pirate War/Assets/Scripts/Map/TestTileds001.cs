91e54d8a9ddaf2713eb5fe8fbf54726e3b30f5bb
Maurilio Reis <mauriliossreis@gmail.com>
2023-02-16 17:33:00 -0300

    Tiled, Scripts, Map
    *************************************************************** Tileds ▼
    
    --------------- Edited and config Tileds map
    
    *************************************************************** Scripts ▼
    
    --------------- Folder: Player ▼
    ----------------------- Folder: Map ▼
    -------------------------------- Edited Script: TestTileds001

--- Pirate War/Assets/Scripts/Map/TestTileds001.cs
+++ Pirate War/Assets/Scripts/Map/TestTileds001.cs
@@ -7,32 +7,74 @@ public class TestTileds001 : MonoBehaviour
 {
     public Grid scenaryGrid;
     public Tilemap[] tilemaps;
-    public TileBase[] tiles;
-    public Transform positionTest;
+    public RuleTile[] tiles;
+
+    [Range(1, 5)]
+    public int widthLand = 4; // largura
+    [Range(1, 50)]
+    public int lengthLand = 5; // comprimento
+    [Range(1, 5)]
+    private int amountLand = 4; // quantidade
 
     Vector3Int positionCell;
 
     void Start()
     {
-        
+        CreatedScenary(positionCell);
     }
 
     void Update()
     {
-        positionCell = scenaryGrid.LocalToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        //if (Input.GetKeyDown(KeyCode.Mouse0))
+        //{
+        //    positionCell = scenaryGrid.LocalToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        //    CreatedScenary(positionCell);
+        //}
 
-        //Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        //if (Input.GetKey(KeyCode.Mouse0))
+        //{
+        //    tilemaps[0].SetTile(positionCell, tiles[1]); // Seta o tile na posicao da celula
+        //}
 
-        Debug.Log(scenaryGrid.LocalToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition))); // Pego a posicao do grid em zeros e uns de acordo com posicao de objeto. (bom para personalizar tbm kk)
-    
-        if (Input.GetKey(KeyCode.Mouse0))
+        //if (Input.GetKey(KeyCode.Mouse1))
+        //{
+        //    tilemaps[0].SetTile(positionCell, tiles[0]); // Seta o tile na posicao da celula
+        //}
+    }
+
+    public void CreatedScenary(Vector3Int clickPositionCell)
+    {
+        positionCell = new Vector3Int((int)Random.Range(-16, 16), (int)Random.Range(-16, 16), 0);
+
+        int currentLengthLand = lengthLand / widthLand;
+
+        int currentCellX = clickPositionCell.x;
+        int currentCellY = clickPositionCell.y;
+
+
+        for (int x =0; x < lengthLand; x++) // if numbers the "for" < widthLand = generate
         {
-            tilemaps[0].SetTile(positionCell, tiles[1]); // Seta o tile na posicao da celula
+            for (int y = 0; y < lengthLand; y++)
+            {
+                float randomDirenctionX = Random.Range(-1.9f, 1.9f);
+                float randomDirenctionY = Random.Range(-1.9f, 1.9f);
+
+                positionCell = new Vector3Int(positionCell.x + (int)randomDirenctionX, positionCell.y + (int)randomDirenctionY, 0);
+
+                for (int i = 0; i < widthLand; i++)
+                {
+                    tilemaps[0].SetTile(positionCell, tiles[1]);
+                    tilemaps[0].SetTile(new Vector3Int(positionCell.x+1, positionCell.y, 0), tiles[1]);
+                    tilemaps[0].SetTile(new Vector3Int(positionCell.x+1, positionCell.y+1, 0), tiles[1]);
+                    tilemaps[0].SetTile(new Vector3Int(positionCell.x, positionCell.y+1, 0), tiles[1]);
+                } 
+            }
         }
 
-        if (Input.GetKey(KeyCode.Mouse1))
+        if (amountLand > 0)
         {
-            tilemaps[0].SetTile(positionCell, tiles[0]); // Seta o tile na posicao da celula
-        }
+            amountLand--;
+            CreatedScenary(positionCell);
+        } 
     }
 }
